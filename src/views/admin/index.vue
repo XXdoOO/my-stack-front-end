8d03172bcb5fbbe8f67bb6e4ca777231d819e11c
25826610264444218 <1972524359@qq.com>
2022-11-30 14:02:06 +0800

    初始化

---
+++ src/views/admin/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+
+</script>
+
+<template>
+  <div>
+    <div></div>
+  </div>
+  <div></div>
+</template>
+
+<style lang="less" scoped>
+div {
+  div {
+    color: red;
+  }
+}
+</style>
---
+++ src/views/admin/log/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="less" scoped>
+
+</style>
