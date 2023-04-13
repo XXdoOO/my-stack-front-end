19ad26c63743ddac08f688d408de023662b67851
谢翔 <1972524359@qq.com>
2023-04-13 16:54:57 +0800

    6

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -68,7 +68,7 @@ onUnmounted(() => {
       </router-link>
 
       <nav>
-        <router-link to="/" :class="{ active: $route.path == '/' }">首页552155</router-link>
+        <router-link to="/" :class="{ active: $route.path == '/' }">首页5522155</router-link>
         <router-link to="/my" :class="{ active: $route.path.split('/')[1] == 'my' }">我的</router-link>
       </nav>
 
