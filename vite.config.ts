55560f5d5a6d06db4c7fdcfdd7c9d44115aa94bd
谢翔 <1972524359@qq.com>
2023-04-15 17:29:13 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 const path = require("path")
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
